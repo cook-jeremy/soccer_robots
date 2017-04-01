39dfd46a29df066b359a57257304f6877a1530dc
Jeremy Cook <jeremycook@utexas.edu>
2017-04-01 10:11:54 -0500

    Removed bad import

--- robot_common/src/color_config.cpp
+++ robot_common/src/color_config.cpp
@@ -2,7 +2,8 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc.hpp>
+//#include <opencv2/imgproc.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 //#include "opencv2/core/version.hpp"
