3e2b20faa67a13a177285858608c31c7c26082f8
Jeremy Cook <jeremycook97@gmail.com>
2017-05-10 12:00:04 -0500

    Added new ip's

--- robot_common/include/robotNames.h
+++ robot_common/include/robotNames.h
@@ -1,4 +1,6 @@
 #include "ros/ros.h"
 #include <string.h>
 
-std::string ROBOT_1_IP = "192.168.0.100";
+std::string ROBOT_1_IP = "192.168.0.101";
+std::string ROBOT_2_IP = "192.168.0.102";
+std::string ROBOT_3_IP = "192.168.0.103";
