2a817be92f10ebd7647aba964e8b496c6c06e783
Jeremy Cook <jeremycook97@gmail.com>
2017-05-11 14:20:16 -0500

    fixed ips

--- robot_common/include/robotNames.h
+++ robot_common/include/robotNames.h
@@ -1,6 +1,6 @@
 #include "ros/ros.h"
 #include <string.h>
 
-std::string ROBOT_1_IP = "192.168.0.201";
-std::string ROBOT_2_IP = "192.168.0.202";
-std::string ROBOT_3_IP = "192.168.0.203";
+std::string ROBOT_1_IP = "192.168.1.201";
+std::string ROBOT_2_IP = "192.168.1.202";
+std::string ROBOT_3_IP = "192.168.1.203";
