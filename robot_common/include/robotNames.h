d0db77ba20646c829f01d7772fde3f8bd6d36696
Jeremy Cook <jeremycook97@gmail.com>
2017-05-11 11:42:02 -0500

    Updated static ip addresses
    Signed-off-by: Jeremy Cook <jeremycook97@gmail.com>

--- robot_common/include/robotNames.h
+++ robot_common/include/robotNames.h
@@ -1,6 +1,6 @@
 #include "ros/ros.h"
 #include <string.h>
 
-std::string ROBOT_1_IP = "192.168.0.101";
-std::string ROBOT_2_IP = "192.168.0.102";
-std::string ROBOT_3_IP = "192.168.0.103";
+std::string ROBOT_1_IP = "192.168.0.201";
+std::string ROBOT_2_IP = "192.168.0.202";
+std::string ROBOT_3_IP = "192.168.0.203";
