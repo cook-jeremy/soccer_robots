65e322985af3a2ed620f5038514fbaf93a355428
Jeremy Cook <jeremycook97@gmail.com>
2017-06-15 21:33:35 -0500

    Working PID with only P term

--- robot_common/src/TaskTurnToTop.cpp
+++ robot_common/src/TaskTurnToTop.cpp
@@ -11,6 +11,7 @@ TaskTurnToTop::TaskTurnToTop(ros::NodeHandle n) {
     last_cmd = "";
     previousError = 0;
     integral = 0;
+    Ku = 3;
 }
 
 void TaskTurnToTop::action(Robot robot, double desiredAngle, float time) {
@@ -19,10 +20,10 @@ void TaskTurnToTop::action(Robot robot, double desiredAngle, float time) {
     double delta = std::atan2(sin(desired-current), cos(desired-current));
     std::cout << "Delta is " << delta << std::endl;
     std::cout << "Time elapsed: " << time << std::endl;
-    double minPower = 200;
-    double maxPower = 300;
+    double minPower = 250;
+    double maxPower = 700;
 
-    if(std::abs(delta) > 0.5) {
+    if(std::abs(delta) > 0.1) {
         //turn left when positive, turn right when negative
         double output = getPIDCorrection(delta, minPower, maxPower, time);
         std::cout << "PID: " << output << std::endl;
@@ -53,17 +54,23 @@ void TaskTurnToTop::action(Robot robot, double desiredAngle, float time) {
         msgS.name = "Robot 1";
         msgS.left_power = 0;
         msgS.right_power = 0;
-        //command_pub.publish(msgS);
+        command_pub.publish(msgS);
         last_cmd = "s";
     }
 }
 
 double TaskTurnToTop::getPIDCorrection(double error, double minPower, double maxPower, int dt) {
-    double Ku = 1;
-    double T = 15;
-    double Kp = Ku*0.6;
-    double Ki = Kp*2/T;
-    double Kd = Kp*T/8;
+    //Ku -= 0.001;
+    double T = 5;
+    double Kp = Ku*0.5;
+    //double Ki = Kp*2/T;
+    double Ki = 0;
+    //double Kd = Kp*T/40;
+    double Kd = 0;
+    std::cout << "Ku = " << Ku << std::endl;
+    std::cout << "Kp = " << Kp << std::endl;
+    std::cout << "Ki = " << Ki << std::endl;
+    std::cout << "Kd = " << Kd << std::endl;
     integral = integral + error*dt;
     double derivative = (error - previousError)/dt;
     double output = Kp*error + Ki*integral + Kd*derivative;
--- robot_common/src/TaskTurnToTop.h
+++ robot_common/src/TaskTurnToTop.h
@@ -14,6 +14,7 @@ class TaskTurnToTop {
     double previousError;
     double integral;
     float previousTime;
+    double Ku;
 public:
     TaskTurnToTop(ros::NodeHandle);
     void action(Robot, double, float);
